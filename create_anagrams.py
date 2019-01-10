15c4857f9b60017db82e1b4f56e0b2c9d865738e
boor <27732675+booranionwhol@users.noreply.github.com>
2019-01-10 22:30:23 +0000

    Add anagram maker

---
+++ create_anagrams.py
@@ -0,0 +1,20 @@
+import random
+import json
+
+qa_dict = []
+with open('capital_cities.txt') as f:
+    lines = f.read().splitlines()
+
+# random.shuffle(lines)
+
+for line in lines:
+    country,city = line.split(',')
+    line = city
+    # TODO: Reshuffle the question if a space is placed at start or end
+    s = ''.join(random.sample(line.lower(), len(line)))
+    print(f'{s} - {line}')
+    question_dict = {s: [line]}
+    qa_dict.append(question_dict)
+
+# TODO: split in to multiple quiz rounds when over X questions
+print(json.dumps(qa_dict))
